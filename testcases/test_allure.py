7000e6bfaee68c94d39269351aca5f6d52a90c9e
Debby <497211215@qq.com>
2025-01-24 14:03:33 +0800

    add trend

--- testcases/test_allure.py
+++ testcases/test_allure.py
@@ -3,8 +3,10 @@
 # @Author : debbyszhang
 # @File : test_case01.py
 # @desc :
+import json
 import os
 import shutil
+from time import time
 
 import allure
 import pytest
@@ -26,19 +28,81 @@ class TestCase03():
     def test_case5(self):
         print('case 05')
 
+    @pytest.mark.xfail(reason='aaa')
     def test_case6(self):
         print('case...')
 
+    @pytest.mark.skip(reason='bbb')
     def test_case7(self):
         print('case7...')
+        assert 1 == 3
+
+    def test_case7(self):
+        print('case7...')
+        assert 1 == 3
+
+# 对生成的Allure报告进行进一步演进（生成一个相对独立的报告静态工程）
+# ALLURE_PLUS_DIR 是存放要生成的报告
+# buildOrder 是表示以构建次数为文件夹名称
+# command = f"allure generate {ALLURE_PATH} -o {os.path.join(ALLURE_PLUS_DIR,str(buildOrder))} --clean"
+# os.system(command)
+def get_dirname(ALLURE_PLUS_DIR):
+    hostory_file = os.path.join(ALLURE_PLUS_DIR, "history.json")
+    if os.path.exists(hostory_file):
+        with open(hostory_file) as f:
+            li = eval(f.read())
+        # 根据构建次数进行排序，从大到小
+        li.sort(key=lambda x: x['buildOrder'], reverse=True)
+        # 返回下一次的构建次数，所以要在排序后的历史数据中的buildOrder+1
+        return li[0]["buildOrder"]+1, li
+    else:
+        # 首次进行生成报告，肯定会进到这一步，先创建history.json,然后返回构建次数1（代表首次）
+        with open(hostory_file, "w") as f:
+            pass
+        return 1, None
+
+
+def update_trend_data(dirname, allure_url,ALLURE_PLUS_DIR, old_data: list):
+    """ dirname：构建次数 old_data：备份的数据 update_trend_data(get_dirname()) """
+    WIDGETS_DIR = os.path.join(ALLURE_PLUS_DIR, f"{str(dirname)}/widgets")
+    # 读取最新生成的history-trend.json数据
+    with open(os.path.join(WIDGETS_DIR, "history-trend.json")) as f:
+        data = f.read()
+
+    new_data = eval(data)
+    if old_data is not None:
+        new_data[0]["buildOrder"] = old_data[0]["buildOrder"]+1
+    else:
+        old_data = []
+        new_data[0]["buildOrder"] = 1
+    # 给最新生成的数据添加reportUrl key，reportUrl要根据自己的实际情况更改
+    new_data[0]["reportUrl"] = f"{allure_url}/{dirname}/index.html"
+    # 把最新的数据，插入到备份数据列表首位
+    old_data.insert(0, new_data[0])
+    # 把所有生成的报告中的history-trend.json都更新成新备份的数据old_data，这样的话，点击历史趋势图就可以实现新老报告切换
+    for i in range(1, dirname+1):
+        with open(os.path.join(ALLURE_PLUS_DIR, f"{str(i)}/widgets/history-trend.json"), "w+") as f:
+            f.write(json.dumps(old_data))
+    # 把数据备份到history.json
+    hostory_file = os.path.join(ALLURE_PLUS_DIR, "history.json")
+    with open(hostory_file, "w+") as f:
+        f.write(json.dumps(old_data))
+    return old_data, new_data[0]["reportUrl"]
 
 
 if __name__ == '__main__':
+    ALLURE_DIR = os.path.dirname(__file__)
+    print(ALLURE_DIR)
+    ALLURE_PATH = os.path.join(ALLURE_DIR, str(int(time())))
+    # command = f'pytest {BASEDIR} -s --alluredir={ALLURE_PATH}'
+    # os.system(command)
     # pytest.main(['-vs', '--alluredir=./result/tmp', __file__])
     pytest.main(['-vs', '--alluredir=./result/tmp', '--clean-alluredir', __file__])
 
     # pytest.main(['-s', __file__])
-    shutil.copy('../environment.xml', './result/tmp')
+    shutil.copy('../allure_reports/environment.xml', './result/tmp')
+    shutil.copy('../allure_reports/categories.json', './result/tmp')
+    shutil.copy('../allure_reports/history-trend.json', './result/tmp')
     pytest.main(['-vs', '--alluredir=./result/tmp', __file__])
     # pytest.main(['-vs', '--alluredir=./result/tmp', '--clean-alluredir', __file__])
 
@@ -47,3 +111,14 @@ if __name__ == '__main__':
 
     # 在线报告
     os.system('allure serve  ./result/tmp')
+
+    # ALLURE_PATH = os.path.join(ALLURE_DIR, str(int(time())))
+    # command = f'pytest {BASEDIR} -s --alluredir={ALLURE_PATH}'
+    # os.system(command)
+    # # 先调用get_dirname()，获取到这次需要构建的次数
+    # buildOrder, old_data = get_dirname()
+    # # 再执行命令行
+    # command = f"allure generate {ALLURE_PATH} -o {os.path.join(ALLURE_PLUS_DIR, str(buildOrder))} --clean"
+    # os.system(command)
+    # # 执行完毕后再调用update_trend_data()
+    # all_data, reportUrl = update_trend_data(buildOrder, old_data)
---
+++ testcases/test_allure_trend.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+# @Time : 2025/1/17 16:07
+# @Author : debbyszhang
+# @File : test_case01.py
+# @desc :
+import ast
+import json
+import os
+import shutil
+from time import time
+
+import allure
+import pytest
+
+
+@allure.feature('测试类')
+class TestCase03():
+
+    @pytest.mark.parametrize('params', ['python', 'java', 'c#'])
+    def test_case4(self, params):
+        print(params)
+
+    sum = 5
+
+    @allure.story('测试用例01')
+    @allure.title('验证case01成功')
+    @allure.description('描述：随便测试一下')
+    @allure.severity(allure.severity_level.MINOR)
+    def test_case5(self):
+        print('case 05')
+
+    @pytest.mark.xfail(reason='aaa')
+    def test_case6(self):
+        print('case...')
+
+    @pytest.mark.skip(reason='bbb')
+    def test_case7(self):
+        print('case7...')
+        assert 1 == 3
+
+    def test_case7(self):
+        print('case7...')
+        assert 1 == 3
+    def test_case8(self):
+        print('case7...')
+        assert 1 == 3
+    def test_case9(self):
+        print('case7...')
+        assert 3 == 3
+    def test_case10(self):
+        print('case7...')
+        assert 3 == 3
+
+
+# 需要执行的测试脚本的路径
+test_path = "./test_allure_trend.py"
+# 需要生成的xml的路径起来干啥呢？小弟弟你还害羞是吧？五角星巴掌，快到这儿打字打字卡到我啦，告诉你怎么回事你搞成啥了？你在做什么？
+allure_xml_path = "../allure_reports/allure_xml"
+# 需要生成测试报告的路径
+allure_path = "../allure_reports/allure_report"
+
+# 获取下一个文件夹的名称，以及最近一个趋势的数据
+def get_dir():
+    print("allure_path", allure_path)
+    # 判断之前是否生成过报告
+    first_path = "../allure_reports/allure_report/1"
+    if os.path.exists(first_path):
+        all_result_dir = os.listdir(allure_path)
+        # 这个地方要注意，如果是mac，listdir获取到一个.DS_store的文件，使用下方的sort会报错，因而要先all_result_dir中将它remove掉
+        all_result_dir.sort(key=int)
+
+        # 取出最近一次执行的历史趋势的json
+        history_file = os.path.join(allure_path, str(int(all_result_dir[-1])), 'widgets', 'history-trend.json')
+        with open(history_file) as f:
+            data = f.read()
+        # 将下一次要生成的文件夹序号以及最新的历史趋势数据返回
+        return int(all_result_dir[-1]) + 1, data
+    else:
+        # 如果之前没有生成过，就创建一个文件夹
+        os.makedirs(os.path.join(allure_path, '1'))
+        return 1, None
+
+
+# 获取最新生成的趋势数据，这个数据里其实只有本次的结果，没有历史的结果
+def update_new_single(buildOrder):
+    new_single_file = os.path.join(allure_path, str(buildOrder), 'widgets', 'history-trend.json')
+    with open(new_single_file, 'r+') as f:
+        # data1 = f.read()
+        data = json.load(f)
+        # 写入本次是第几次执行、测试报告的路径
+        data[0]["buildOrder"] = int(buildOrder)
+        data[0]['reportUrl'] = f'http://localhost:63343/ttx_ofs_autoapi/allure_report/{str(buildOrder)}/index.html'
+    with open(new_single_file, 'w+') as f:
+        json.dump(data, f)
+
+
+# 重写新生成的history-trend.json文件，用历史+本次=最新
+def update_file(buildOrder):
+    old_data = os.path.join(allure_path, str(int(buildOrder) - 1), 'widgets', 'history-trend.json')
+    new_data = os.path.join(allure_path, str(int(buildOrder)), 'widgets', 'history-trend.json')
+    with open(old_data) as f:
+        old = json.load(f)
+        dict = []
+        for i in range(len(old)):
+            dict.append(old[i])
+        print(dict)
+    with open(new_data) as f:
+        r = f.read()
+        new_list = ast.literal_eval(r)
+        for i in range(len(dict)):
+            new_list.append(dict[i])
+    with open(new_data, 'w') as f:
+        json.dump(new_list, f)
+
+
+if __name__ == '__main__':
+    print(allure_path)
+    buildOrder, old_data = get_dir()
+    # 先使用command生成xml文件
+    shutil.copy('../allure_reports/environment.xml', allure_xml_path)
+    print(os.path.join(allure_xml_path, str(buildOrder)))
+    command = f'pytest {test_path} -s --alluredir={os.path.join(allure_xml_path, str(buildOrder))}'
+    os.system(command)
+    # shutil.copy('../allure_reports/categories.json', allure_xml_path)
+    # 再使用command1由xml生成json文件的测试报告
+    command1 = f'allure generate {os.path.join(allure_xml_path, str(buildOrder))} -o {os.path.join(allure_path, str(buildOrder))} --clean'
+    print(command1)
+    os.system(command1)
+    update_new_single(buildOrder)
+    if buildOrder != 1:
+        update_file(buildOrder)
+
+    # ALLURE_DIR = os.path.dirname(__file__)
+    # print(ALLURE_DIR)
+    # ALLURE_PATH = os.path.join(ALLURE_DIR, str(int(time())))
+    # # command = f'pytest {BASEDIR} -s --alluredir={ALLURE_PATH}'
+    # # os.system(command)
+    # # pytest.main(['-vs', '--alluredir=./result/tmp', __file__])
+    # pytest.main(['-vs', '--alluredir=./result/tmp', '--clean-alluredir', __file__])
+    #
+    # # pytest.main(['-s', __file__])
+    # shutil.copy('../allure_reports/environment.xml', './result/tmp')
+    # shutil.copy('../allure_reports/categories.json', './result/tmp')
+    # shutil.copy('../allure_reports/history-trend.json', './result/tmp')
+    # pytest.main(['-vs', '--alluredir=./result/tmp', __file__])
+    # # pytest.main(['-vs', '--alluredir=./result/tmp', '--clean-alluredir', __file__])
+    #
+    # # 离线报告
+    # os.system('allure generate  ./result/tmp -o ./result/offline --clean')
+    #
+    # # 在线报告
+    # os.system('allure serve  ./result/tmp')
+
+
